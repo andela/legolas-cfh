79216bc0ad1da0cd05617578cf8ce6303bdea25c
Daniel James <daniel.james@andela.com>
2017-04-13 11:34:24 +0100

    Revert "#143342715 Add app.json for heroku review app"

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ var express = require('express'),
     fs = require('fs'),
     passport = require('passport'),
     logger = require('mean-logger'),
-    dotenv = require('dotenv'),
     io = require('socket.io');
-
-// Set Dotenv Config
-dotenv.config();
 
 /**
  * Main application entry file.
