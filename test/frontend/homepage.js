5470402a223c2ea88c259494b79c456678279083
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-04-26 17:22:01 +0100

    Write more tests and make them pass

--- test/frontend/homepage.js
+++ test/frontend/homepage.js
@@ -30,8 +30,24 @@ describe('Our HTML Page', () => {
     assert.equal(window.$('h1').text(), 'Donate Now', 'Make sure to set the content of your `h1` element to "Donate Now".');
   });
 
-  it('should have at least 6 buttons @.btn', () => {
-    assert.isAtLeast(window.$('.btn').length(), 6, 'Make sure you have up to 6 buttons on the page.');
+  it('should lead to sign up link the sign up / sign in @a', () => {
+    assert.equal(window.$('#sign-up-test').attr('href'), '#!/signup', 'Ensure this links to the sign up page');
+  });
+
+  it('should lead to the gaming screen @a', () => {
+    assert.equal(window.$('#game-test-link').attr('href'), '/play', 'Ensure this links to the game');
+  });
+
+  it('should have video content on donation @iframe', () => {
+    assert.equal(window.$('iframe').attr('src'), 'https://www.youtube.com/embed/IaC25klxZyg', 'Ensure a video on charity is included in the charity section.');
+  });
+
+  it('should send the user to the right link  @#payment-link', () => {
+    assert.equal(window.$('#payment-link').attr('href'), 'https://www.crowdrise.com/donate/project/cfhio/cards4humanity/?widget=true', 'Make sure the "Donate Now" button links to the right url.');
+  });
+
+  it('should have at least 6 buttons @btn', () => {
+    assert.isAbove(window.$('.btn').length, 5, 'Make sure you more than 5 buttons on the page.');
   });
 
   it('should have a ul element for the menu @ul', () => {
@@ -39,7 +55,11 @@ describe('Our HTML Page', () => {
   });
 
   it('should have at least 10 li elements as menu items @li', () => {
-    assert.isAtLeast(window.$('li').length, 10, 'Make sure to create at least 10 `li` elements as menu items.');
+    assert.isAbove(window.$('li').length, 8, 'Make sure to create more than 8 `li` elements as menu items.');
+  });
+
+  it('Footer section should have footer element @footer', () => {
+    assert.isAtLeast(window.$('footer').length, 1, 'Make sure to create an `footer` element.');
   });
 
   it('should have content for all `li` elements. @li', () => {
