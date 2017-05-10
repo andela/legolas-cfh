1f091b31d05c661204b5f31f2adda637e4ae4774
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-05-10 14:34:23 +0100

    [feature #143342755] update the game and question files.

--- config/socket/game.js
+++ config/socket/game.js
@@ -383,9 +383,9 @@ Game.prototype.removePlayer = function(thisPlayer) {
       // If the player is the czar...
       // If players are currently picking a card, advance to a new round.
       if (this.state === 'czar pick card') {
-        // this.startNextRound(this);
+        this.startNextRound(this);
         // this.changeCzar(this);
-        this.state = 'czar pick card';
+        // this.state = 'czar pick card';
         console.log('this.state----', this.state);
       } else if (this.state === "waiting for players to pick") {
         clearTimeout(this.choosingTimeout);
