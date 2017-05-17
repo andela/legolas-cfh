09d61af4c7091f882e9bc259ff4e875c5a99e7aa
Obinna Okwuolisa <andeladeveloper@Andelas-MacBook-Pro-7.local>
2017-05-17 19:46:27 +0100

    [feature #143342759] add updated styles

--- public/js/chat_box.js
+++ public/js/chat_box.js
@@ -10,4 +10,3 @@ $('body').on('click', '#startChat, .emojionearea-editor', () => {
   $('.chat-content').slideDown();
 });
 $('body').on('click', '.chatbox', () => $('#chatNotification').hide());
-//MONGOHQ_URL=mongodb://heroku_wl7x6cjf:buqkcc7m11iuiivaofl6nfm3ti@ds159670.mlab.com:59670/heroku_wl7x6cjf
